b6bec6fbab607a1b31bc76065fc1b473a0a800d5
bgreen53 <green.barry53@gmail.com>
2020-02-18 23:14:31 -0700

    changed position of img and text, added music

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 window.onload = function () {
 
     var words = [
-        "SNOW WHITE", "ANNA",
+        "STITCH", "ANNA",
         "CINDERELLA", "ELSA",
         "AURORA", "RAPUNZEL",
         "ARIEL", "BAYMAX",
@@ -51,64 +51,339 @@ window.onload = function () {
         
     }
 
+
+    //vars to call audio
+    var lilo = document.getElementById("lilo")
+    var an = document.getElementById("an")
+    var cin = document.getElementById("cin")
+    var letgo = document.getElementById("letgo")
+    var sb = document.getElementById("sb")
+    var tangled = document.getElementById("rap")
+    var poyw = document.getElementById("part")
+    var bay = document.getElementById("bay")
+    var beauty = document.getElementById("bnb")
+    var deep = document.getElementById("dig")
+    var carpet = document.getElementById("newworld")
+    var illgo = document.getElementById("far")
+    var wincol = document.getElementById("wind")
+    var demi = document.getElementById("welcome")
+    var ref = document.getElementById("reflect")
+    var likeme = document.getElementById("friend")
+
+
     
-//Pic of guessed characters
+//Pic and song of guessed characters
     function pic() {
         
         if (word === words[0]) {
-            document.getElementById("image").src = "./assets/images/snow.jpg";
+            lilo.play()
+            an.pause()
+            cin.pause()
+            letgo.pause()
+            sb.pause()
+            tangled.pause()
+            poyw.pause()
+            bay.pause()
+            beauty.pause()
+            deep.pause()
+            carpet.pause()
+            illgo.pause()
+            wincol.pause()
+            demi.pause()
+            ref.pause()
+            likeme.pause()
+            
+            document.getElementById("image").src = "./assets/images/stitch.jpg";
         }
         else if (word === words[1]){
+            an.play()
+            lilo.pause()
+            cin.pause()
+            letgo.pause()
+            sb.pause()
+            tangled.pause()
+            poyw.pause()
+            bay.pause()
+            beauty.pause()
+            deep.pause()
+            carpet.pause()
+            illgo.pause()
+            wincol.pause()
+            demi.pause()
+            ref.pause()
+            likeme.pause()
             document.getElementById("image").src = "./assets/images/anna.jpg";
         }
         else if (word === words[2]){
-           
+           cin.play()
+           lilo.pause()
+            an.pause()
+            letgo.pause()
+            sb.pause()
+            tangled.pause()
+            poyw.pause()
+            bay.pause()
+            beauty.pause()
+            deep.pause()
+            carpet.pause()
+            illgo.pause()
+            wincol.pause()
+            demi.pause()
+            ref.pause()
+            likeme.pause()
             document.getElementById("image").src = "./assets/images/cinder.jpg";
         }
         else if (word === words[3]){
-            
+            letgo.play()
+            lilo.pause()
+            an.pause()
+            cin.pause()
+            sb.pause()
+            tangled.pause()
+            poyw.pause()
+            bay.pause()
+            beauty.pause()
+            deep.pause()
+            carpet.pause()
+            illgo.pause()
+            wincol.pause()
+            demi.pause()
+            ref.pause()
+            likeme.pause()
             document.getElementById("image").src = "./assets/images/elsa.jpg";
         }
         else if (word === words[4]){
-            
+            sb.play()
+            lilo.pause()
+            an.pause()
+            cin.pause()
+            letgo.pause()
+            tangled.pause()
+            poyw.pause()
+            bay.pause()
+            beauty.pause()
+            deep.pause()
+            carpet.pause()
+            illgo.pause()
+            wincol.pause()
+            demi.pause()
+            ref.pause()
+            likeme.pause()
             document.getElementById("image").src = "./assets/images/aurora.jpg";
         }
         else if (word === words[5]){
-           
+            tangled.play()
+            lilo.pause()
+            an.pause()
+            cin.pause()
+            letgo.pause()
+            sb.pause()
+            poyw.pause()
+            bay.pause()
+            beauty.pause()
+            deep.pause()
+            carpet.pause()
+            illgo.pause()
+            wincol.pause()
+            demi.pause()
+            ref.pause()
+            likeme.pause()
             document.getElementById("image").src = "./assets/images/rapunzel.jpg";
         }
         else if (word === words[6]){
+            poyw.play()
+            lilo.pause()
+            an.pause()
+            cin.pause()
+            letgo.pause()
+            sb.pause()
+            tangled.pause()
+            bay.pause()
+            beauty.pause()
+            deep.pause()
+            carpet.pause()
+            illgo.pause()
+            wincol.pause()
+            demi.pause()
+            ref.pause()
+            likeme.pause()
             document.getElementById("image").src = "./assets/images/ariel.jpg";
         }
         else if (word === words[7]){
+            bay.play()
+            lilo.pause()
+            an.pause()
+            cin.pause()
+            letgo.pause()
+            sb.pause()
+            tangled.pause()
+            poyw.pause()
+            beauty.pause()
+            deep.pause()
+            carpet.pause()
+            illgo.pause()
+            wincol.pause()
+            demi.pause()
+            ref.pause()
+            likeme.pause()
             document.getElementById("image").src = "./assets/images/baymax.jpg";
         }
         else if (word === words[8]){
+            lilo.pause()
+            an.pause()
+            cin.pause()
+            letgo.pause()
+            sb.pause()
+            tangled.pause()
+            poyw.pause()
+            bay.pause()
+            beauty.play()
+            deep.pause()
+            carpet.pause()
+            illgo.pause()
+            wincol.pause()
+            demi.pause()
+            ref.pause()
+            likeme.pause()
             document.getElementById("image").src = "./assets/images/belle.jpg";
         }
         else if (word === words[9]){
+            lilo.pause()
+            an.pause()
+            cin.pause()
+            letgo.pause()
+            sb.pause()
+            tangled.pause()
+            poyw.pause()
+            bay.pause()
+            beauty.pause()
+            deep.play()
+            carpet.pause()
+            illgo.pause()
+            wincol.pause()
+            demi.pause()
+            ref.pause()
+            likeme.pause()
             document.getElementById("image").src = "./assets/images/tiana.jpg";
         }
         else if (word === words[10]){
+            lilo.pause()
+            an.pause()
+            cin.pause()
+            letgo.pause()
+            sb.pause()
+            tangled.pause()
+            poyw.pause()
+            bay.pause()
+            beauty.pause()
+            deep.pause()
+            carpet.play()
+            illgo.pause()
+            wincol.pause()
+            demi.pause()
+            ref.pause()
+            likeme.pause()
             document.getElementById("image").src = "./assets/images/jasmine.jpg";
         }
         else if (word === words[11]){
+            lilo.pause()
+            an.pause()
+            cin.pause()
+            letgo.pause()
+            sb.pause()
+            tangled.pause()
+            poyw.pause()
+            bay.pause()
+            beauty.pause()
+            deep.pause()
+            carpet.pause()
+            illgo.play()
+            wincol.pause()
+            demi.pause()
+            ref.pause()
+            likeme.pause()
             document.getElementById("image").src = "./assets/images/moana.jpg";
         }
         else if (word === words[12]){
+            lilo.pause()
+            an.pause()
+            cin.pause()
+            letgo.pause()
+            sb.pause()
+            tangled.pause()
+            poyw.pause()
+            bay.pause()
+            beauty.pause()
+            deep.pause()
+            carpet.pause()
+            illgo.pause()
+            wincol.play()
+            demi.pause()
+            ref.pause()
+            likeme.pause()
             document.getElementById("image").src = "./assets/images/poca.jpg";
         }
         else if (word === words[13]){
+            lilo.pause()
+            an.pause()
+            cin.pause()
+            letgo.pause()
+            sb.pause()
+            tangled.pause()
+            poyw.pause()
+            bay.pause()
+            beauty.pause()
+            deep.pause()
+            carpet.pause()
+            illgo.pause()
+            wincol.pause()
+            demi.play()
+            ref.pause()
+            likeme.pause()
             document.getElementById("image").src = "./assets/images/maui.jpg";
         }
         else if (word === words[14]){
+            lilo.pause()
+            an.pause()
+            cin.pause()
+            letgo.pause()
+            sb.pause()
+            tangled.pause()
+            poyw.pause()
+            bay.pause()
+            beauty.pause()
+            deep.pause()
+            carpet.pause()
+            illgo.pause()
+            wincol.pause()
+            demi.pause()
+            ref.play()
+            likeme.pause()
             document.getElementById("image").src = "./assets/images/mulan.jpg";
         }
         else if (word === words[15]){
+            lilo.pause()
+            an.pause()
+            cin.pause()
+            letgo.pause()
+            sb.pause()
+            tangled.pause()
+            poyw.pause()
+            bay.pause()
+            beauty.pause()
+            deep.pause()
+            carpet.pause()
+            illgo.pause()
+            wincol.pause()
+            demi.pause()
+            ref.pause()
+            likeme.play()
             document.getElementById("image").src = "./assets/images/genie.jpg";
         }
        
     }
+
+    //reset defaults
     function reset() {
          maxGuess = 10;
          wrongGuess = [];
@@ -117,20 +392,17 @@ window.onload = function () {
         Game()
     } 
 
-
+//check user guess against word
     function checkLetters(letter) {
         var letterInWord = false;
         
-       
-        //if the generated randomword is equal to the letter entered... then variable is true
         for (var i = 0; i < blanks; i++) {
             if (word[i] == letter) {
                 letterInWord = true;
             }
         }
-        //if letterInWord (false)
+        
         if (letterInWord) {
-            //check each letter to see if it matches word
             for (var i = 0; i < blanks; i++) {
                 if (word[i] == letter) {
                     blanksText[i] = letter;
@@ -165,7 +437,7 @@ window.onload = function () {
             //display wins on screen
             document.getElementById("wins").innerHTML = "Wins: " + wins;
     
-            //if LOST...then alert and reset new round
+            //if loss then add to losses and reset
         } else if (maxGuess === 0) {
             losses++;
             pause = true
@@ -182,23 +454,19 @@ window.onload = function () {
     } 
     
 
+    //call game function
     Game()
        
 
    
-
+//allow user guess and check against word
    document.onkeyup = function (event) {
 
     if(isAlpha(event.key) && !pause){
     var guesses = String.fromCharCode(event.keyCode).toUpperCase();
-    //check to see if guess entered matches value of random word
     checkLetters(guesses);
-    //process wins/loss 
     complete();
-    //store player guess in console for reference 
     console.log(guesses);
-
-    //display/store incorrect letters on screen
     document.getElementById("letguess").innerHTML = "  " + wrongGuess.join(" ");
 }
 }
